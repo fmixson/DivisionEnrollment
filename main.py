aa0da252b09dc17131a234bf587182f7c68bb447
fmixson <flmixson@cerritos.edu>
2022-02-17 09:03:11 -0800

    2/17/22 commit

--- main.py
+++ main.py
@@ -152,30 +152,24 @@ class GroupDepartments:
 s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
 driver.get('https://secure.cerritos.edu/schedule/')
-
 check_all = driver.find_element(By.XPATH, '/html/body/form/table[1]/tbody/tr[1]/td[1]/label/input').click()
 check_LA = driver.find_element(By.XPATH, '/html/body/form/table[6]/tbody/tr[5]/td[2]/label/input').click()
-
 click_View = driver.find_element(By.XPATH, '/html/body/form/p[4]/input').click()
 page_loading = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ASL110descs')))
 headers = ['Dept', 'Course', 'Session', 'Class', 'Start', 'End', 'Days', 'Room', 'Size', 'Max', 'Wait', 'Cap', 'Seats',
            'WaitAv', 'Status', 'Instructor', 'Type', 'Hours', 'Books', 'Modality']
 pd.set_option('display.max_columns', None)
 enrollment_df = pd.DataFrame(columns=headers)
-
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'lxml')
-
 table_count = 0
 h2_source = soup.find_all('h2')
 session_source = soup.find_all('tr', {'class': 'sess1head', 'colspan': '14'})
 'Each table consists of a course section'
 tables = soup.find_all(['table', {'cellspacing': '0', 'class': 'class'}])
 
-
 for table in tables:
     'The for loop extracts from each table information about the course'
-    # print('table', table.prettify())
     c = CourseName(html_table=h2_source)
     course_name, department = c.pull_course_name()
     t = TableWork(html_table=table, course_name=course_name, department=department)
@@ -184,25 +178,7 @@ for table in tables:
 d = DataframeWork(enrollment_df=enrollment_df)
 d.sheet_integers()
 df_groupby_dept=d.lecture_only()
-# enrollment_df['Size'] = pd.to_numeric(enrollment_df['Size'], errors='coerce').fillna(0).astype('int')
-# enrollment_df['Max'] = pd.to_numeric(enrollment_df['Max'], errors='coerce').fillna(0).astype('int')
-# enrollment_df['Hours'] = pd.to_numeric(enrollment_df['Hours'], errors='coerce').fillna(0).astype('int')
 
-# lecture_df = enrollment_df['Type'] == 'Lecture'
-# lecture_enrollment_df = enrollment_df[lecture_df]
-#
-# lecture_enrollment_df.loc[lecture_enrollment_df['Room'] == 'REMOTE', 'Modality'] = 'Remote Course'
-# lecture_enrollment_df.loc[lecture_enrollment_df['Room'] == 'ONLINE', 'Modality'] = 'Online Course'
-# rooms = ['LA106', 'LA109', 'LA201', 'SS207', 'LA213', 'LC218', 'LA110', 'SS211', 'SS225', 'LA103', 'SS224', 'LC217',
-#          'LA211', 'LA202']
-# for room in rooms:
-#     lecture_enrollment_df.loc[lecture_enrollment_df['Room'] == room, 'Modality'] = 'Hybrid Course'
-# lecture_enrollment_df.loc[lecture_enrollment_df['Max'] == 15, 'Modality'] = 'In Person'
-# df_groupby_dept = lecture_enrollment_df.groupby(['Dept'])
-#
-#
-# lecture_enrollment_df['FTES'] = lecture_enrollment_df['Size'] * (((lecture_enrollment_df['Hours']/18)*17.5)/525)
-# lecture_enrollment_df.to_excel('Division_Enrollment.xlsx')
 
 departments = ['AFRS', 'ASL', 'CHIN', 'COMM', 'ENGL', 'ESL', 'FREN', 'GERM', 'JAPN', 'READ', 'SPAN']
 for department in departments:
